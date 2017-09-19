1bd8fd381ba6b91a9048a39c7b420447b5097a05
srkollur <shivam.kollur@gmail.com>
2017-09-18 23:29:35 -0400

    Add files via upload

--- Test_Removing Different Dates.py
+++ Test_Removing Different Dates.py
@@ -21,13 +21,15 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
     i = 0
 
     while i < len(dates1):
-        dates1[i] = dates1[i].date().strftime('%m-%d-%Y')
+        if type(dates2[i]) is datetime:
+            dates1[i] = str(dates1[i].date().strftime('%m-%d-%Y'))
         i = i + 1
 
     i = 0
 
     while i < len(dates2):
-        dates2[i] = dates2[i].date().strftime('%m-%d-%Y')
+        if type(dates2[i]) is datetime:
+            dates2[i] = str(dates2[i].date().strftime('%m-%d-%Y'))
         i = i + 1
 
     index = 0
@@ -65,8 +67,8 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
     print(len(toRemoveFrom1))
     if len(toRemoveFrom1) > 0:
         while removeIndex < len(toRemoveFrom1):
-            print(removeIndex)
-            dates1.remove(dates1[toRemoveFrom1[removeIndex]])
+            r = toRemoveFrom1[removeIndex]
+            dates1.remove(dates1[r])
             removeIndex = removeIndex + 1
 
     #dates1 list is modified
@@ -77,7 +79,8 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
 
     if len(toRemoveFrom1) > 0:
         while removeIndex < len(toRemoveFrom1):
-            var1.remove(var1[toRemoveFrom1[removeIndex]])
+            r = toRemoveFrom1[removeIndex]
+            var1.remove(var1[r])
             removeIndex = removeIndex + 1
 
     #var1 list is modified
@@ -86,7 +89,8 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
 
     if len(toRemoveFrom2) > 0:
         while removeIndex < len(toRemoveFrom2):
-            dates2.remove(dates2[toRemoveFrom2[removeIndex]])
+            r = toRemoveFrom2[removeIndex]
+            dates2.remove(dates2[r])
             removeIndex = removeIndex + 1
 
     #dates2 list is modified
@@ -97,7 +101,8 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
 
     if len(toRemoveFrom2) > 0:
         while removeIndex < len(toRemoveFrom2):
-            var2.remove(var2[toRemoveFrom2[removeIndex]])
+            r = toRemoveFrom2[removeIndex]
+            var2.remove(var2[r])
             removeIndex = removeIndex + 1
 
     #var2 list is modified
@@ -113,12 +118,35 @@ def removeDifferentDates(df1, df2, columnName1, columnName2):
     return final
 
 skewdf = quandl.get("CBOE/SKEW", authtoken="HMbMEXCjrykoHnyB6PVC", start_date = "2006-12-13", end_date = "2017-08-24")
-IWM_df = wb.DataReader('IWM', 'google', '2006-12-13', "2017-08-24")
+
+VIXurl = 'http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv'
+VIX_df = pd.DataFrame(pd.read_csv(VIXurl))
+
+VIX_values = VIX_df["Unnamed: 4"].tolist()
+
+VIX_values.remove(VIX_values[0])
+
+series = (VIX_df["CBOE data is compiled for the convenience of site visitors and is furnished without responsibility for accuracy and is accepted by the site visitor on the condition that transmission or omissions shall not be made the basis for any claim demand or cause for action.  Your use of CBOE data is subject to the Terms and Conditions of CBOE's Websites."])
+
+VIX_list = series.values.tolist()
+
+label = VIX_list[0]
+
+VIX_list.remove(VIX_list[0])
+
+VIX_df = pd.DataFrame({label: VIX_list, "VIX": VIX_values})
+
+VIX_df = VIX_df.set_index('Date')
+
+print(VIX_df)
 
 print(skewdf)
-print(IWM_df)
 
-l = removeDifferentDates(skewdf, IWM_df, 'Skew', 'IWM')
 
-print(skewdf.index == IWM_df.index)
+#print(VIX_df.columns.values)
 
+l = removeDifferentDates(skewdf, VIX_df, 'Skew', 'VIX')
+
+print(skewdf.index == VIX_df.index)
+
+#CONVERT Y/M/D TO Y-M-D
